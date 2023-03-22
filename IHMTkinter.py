0d10c5d9197a3c81915ff84c00c565634dfb2de0
PierreBoivent <128586875+PierreBoivent@users.noreply.github.com>
2023-03-22 12:25:14 +0100

    Add dynamic graph - Delete other tkinter items
    Suppression des autres items, mais ajout du graph dynamique dans l'interface Tkinter

--- IHMTkinter.py
+++ IHMTkinter.py
@@ -14,22 +14,6 @@ port='/dev/ttyACM0'
 baudrate=115200
 serBuffer = ""
 
-#TODO Tester avec connexion au STM32
-#TODO revoir toute l'organisationd des emplacements des modules
- 
-########################################################################
-#                        FUNCTIONS
-########################################################################
-    
-def onPushTest():
-    var=int(strEstimated.get())
-    strEstimated.set(var+1)
-    print("value récupérée du checkbutton du moteur : ")
-    print(moteurButton.getVar())
-    if(moteurButton.get()==1):
-        moteurButton.setvar(0)
-    else:
-        moteurButton.setvar(1)
  
 ########################################################################
 #                             MAIN
@@ -47,63 +31,38 @@ if  __name__ == "__main__" :
     ui=Tk()
  
     ui.title("IPS - Déformation par jauge")
-    ui.geometry("1000x500")
-    #ui.resizable(width=0, height=0)
+    ui.geometry("400x400")
+
+    #GRAPH
     
-    
-    #zone avec graphique de la déformation en fonction de la donnée reçu
-    #essayer de faire le graphique évolutif
-    ui.graph = Frame(ui, width=150, bg="#ababab")
-    ui.graph.grid(row=0, column=0, rowspan=2, sticky="ns")
-    
+    k = 2*np.pi
+    w = 2*np.pi
+    dt = 0.001
+
+    xmin = 0
+    xmax = 100
+    nbx = 100
+    x = np.linspace(xmin, xmax, nbx)
+
+    fig = plt.figure() # initialise la figure
+    line, = plt.plot([], []) 
+    plt.xlim(xmin, xmax)
+    plt.ylim(298, 302)
+
+    def animate(i): 
+        t = i * dt
+        y = np.cos(k*x -w*t ) + 300
+        line.set_data(x, y)
+        return line,
+
+    ani = animation.FuncAnimation(fig, animate, frames=10000,
+                                interval=1, blit=True, repeat=False)
+   
     
     matplotlib.use("TkAgg")
-    figure = Figure(figsize=(5,5),dpi=100)
-    plot = figure.add_subplot(1,1,1)
-    plot.plot(0.5,0.3)
-    canvas = FigureCanvasTkAgg(figure, ui.graph)
-    canvas.get_tk_widget().grid(row=0, column=0)
-    
-    #Label : estimation de poids
-    labelWeightEstimation=Label(ui,text="Estimation du poids (en g)", font=("Arial",15), fg="black")
-    labelWeightEstimation.grid(row=0, column=1)
-    #labelWeightEstimation.pack()
-    
-    #zone de texte avec "poid : *zone de texte modifiable* g"
-    strEstimated=StringVar(value="0")
-    WeightEntry=Entry(ui, textvariable=strEstimated, font=("Arial",15), fg="black")
-    WeightEntry.grid(row=0,column=2)
-    #WeightEntry.pack()
-    
-    testButton=Button(ui, text="add +1", font=("Arial",12), background="seagreen", fg="black", bd=2, relief=RAISED, command=onPushTest)
-    testButton.grid(row=1,column=1)
-    #avoir une zone "moteur"
-    ui.servomoteur=Frame(ui, width=150, bg="#ababab")
-    ui.servomoteur.grid(row=0, column=2)
-    #une case cochable pour indiquer si le moteur est en train  de tourner
-    moteurButton=Checkbutton(ui, text="Moteur en route", height=10, width=10)
-    moteurButton.grid(row=2, column=2)
-    #avoir une zone qui indique la vitesse envoyée au moteur
-    
-    
-    labelToSendMes=Label(ui, text="Message to Send", font=("Arial", 10), fg="black")
-    #labelToSendMes.pack()
- 
-    strToSend=StringVar()
-    toSendEntry=Entry(ui, textvariable=strToSend)
-    #toSendEntry.pack()
- 
-    #copyButton=Button(ui, text="SEND", font=("Arial",10, "bold"), bg="seagreen3", fg="black", bd=3, relief=RAISED, command=onPushSend)
-    #copyButton.pack()
-         
-    labelReceivedMes=Label(ui, text="Received Message", font=("Arial", 10), fg="black")
-    #labelReceivedMes.pack()
- 
-    strReceived=StringVar()
-    receivedEntry=Entry(ui, textvariable=strReceived)
-    #receivedEntry.pack()
+    canvas = FigureCanvasTkAgg(fig, ui)
+    canvas.get_tk_widget().pack()
  
     #ui.after(1,receive)
     ui.mainloop()       # MAIN LOOP
- 
-########################################################################
+
