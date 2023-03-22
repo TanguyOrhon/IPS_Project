fc58aec4f7bd996866045cbb1c2970b8e56c243d
Tanguy Orhon <74053961+TanguyOrhon@users.noreply.github.com>
2023-03-22 14:35:27 +0100

    Delete IHM directory

--- IHM/stm.py
+++
@@ -1,7 +0,0 @@
-# try:
-    #    ser = serial.Serial(port, baudrate, bytesize=8, parity='N', stopbits=1, timeout=None, rtscts=False, dsrdtr=False)
-    #    print("serial port " + ser.name + " opened")
-    # except Exception:
-    #    print("error open serial port: " + port)
-    #    exit()
-# ui.after(1,receive)
