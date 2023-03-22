b3a56d80a0a50fa1f4e6ff824300de796124e8a2
TanguyOrhon <tanguy.orhon32@gmail.com>
2023-03-22 14:43:34 +0100

    Init commit STM + IHM

---
+++ IHM/func.py
@@ -0,0 +1,49 @@
+import serial
+import time
+from tkinter import *
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib
+
+
+class Func:
+    def __init__(self):
+        self.strEstimated = StringVar(value="0")
+        self.x = IntVar()
+
+    def graph(self, ui):
+        matplotlib.use("TkAgg")
+        figure = Figure(figsize=(5, 5), dpi=100)
+        plot = figure.add_subplot(1, 1, 1)
+        plot.plot(0.5, 0.3)
+        canvas = FigureCanvasTkAgg(figure, ui.graph)
+        canvas.get_tk_widget().grid(row=0, column=0)
+
+    def weight(self, ui):
+        # Label : estimation de poids
+        labelWeightEstimation = Label(ui, text="Estimation du poids (en g)", font=("Arial", 15), fg="black")
+        labelWeightEstimation.grid(row=0, column=1)
+        # labelWeightEstimation.pack()
+
+        # zone de texte avec "poid : *zone de texte modifiable* g"
+        WeightEntry = Entry(ui, textvariable=self.strEstimated, font=("Arial", 15), fg="black")
+        WeightEntry.grid(row=0, column=2)
+        # WeightEntry.pack()
+
+    def button(self, ui):
+        moteurButton = Checkbutton(ui, text="Moteur en route", height=10, width=10, variable=self.x)
+        moteurButton.grid(row=2, column=2)
+        testButton = Button(ui, text="add +1", font=("Arial", 12), background="seagreen", fg="black", bd=2,
+                            relief=RAISED,
+                            command=self.onPushTest)
+        testButton.grid(row=1, column=1)
+
+    def onPushTest(self):
+        var = int(self.strEstimated.get())
+        self.strEstimated.set(var + 1)
+        print("value récupérée du checkbutton du moteur : ")
+        print(self.x.get())
+        self.x.set(not self.x.get())
